773e4a1dba61bd8e08dead99e3513bd589c50925
mohammed cheikh <mohammed.cheikh.ensao@ump.ac.ma>
2026-07-20 15:45:31 +0100

    Update database connection to use environment variables
    Changed database connection credentials to use environment variables for user and password.

--- AppJAVA/AgenceVoyage3/src/database/Database.java
+++ AppJAVA/AgenceVoyage3/src/database/Database.java
@@ -4,15 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Database {
-
     private static Connection conn = null;
 
- 
-    private static String url      = "jdbc:mysql://localhost:3306/agence_voyage";
-    private static String user     = "root";
-    private static String password = "";
+    private static final String url  = "jdbc:mysql://localhost:3306/agence_voyage";
+    private static final String user = System.getenv().getOrDefault("DB_USER", "root");
+    private static final String password = System.getenv("DB_PASSWORD");
 
-    public static Connection getConnection() {                   
+    public static Connection getConnection() {
         if (conn == null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
