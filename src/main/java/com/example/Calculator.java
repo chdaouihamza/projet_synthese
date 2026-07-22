518fad0ee6bf51a532e136fa5163a309d44a4112
mohammed cheikh <mohammed.cheikh.ensao@ump.ac.ma>
2026-07-22 16:00:16 +0100

    Add files via upload

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,26 @@
+package com.example;
+
+/**
+ * Simple calculator used to validate the SonarQube setup.
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return (double) a / b;
+    }
+}
